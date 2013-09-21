aeda79031b9dee14692a26752cf7a721aa097369
Arturo Escudero <darkturo@gmail.com>
2013-09-21 14:02:09 +0200

    New test added, now we want that when countdown is started the instance of countdown changes its state

--- Katas/ReloadCountdown/CountdownTest.py
+++ Katas/ReloadCountdown/CountdownTest.py
@@ -3,9 +3,14 @@ import unittest
 from Countdown import *
 
 class CountdownTest(unittest.TestCase):
-   def test_initial_state_of_countdown(self):
+   def test_initial_state_of_countdown_should_be_isStopped(self):
       counter = Countdown()
       self.assertTrue(counter.isStopped())
+
+   def test_after_starting_the_countdown_isStopped_is_false(self):
+      counter = Countdown()
+      counter.startCountDown(10)
+      self.assertFalse(counter.isStopped())
 
 if __name__ == '__main__':
    unittest.main()
